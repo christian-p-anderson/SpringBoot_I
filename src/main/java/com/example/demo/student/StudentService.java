0cbaa737d34fc3de19806cc7ad7bb770ba7c7dc2
Christian Anderson <christian.p.anderson1@gmail.com>
2021-06-24 13:04:30 -0400

    created Controller and Service classes to seperate concerns

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,31 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+
+                new Student (
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                ),
+                new Student (
+                        2L,
+                        "Christian",
+                        "ca@gmail.com",
+                        LocalDate.of(2000, Month.JUNE, 9),
+
+
+                        21
+                )
+        );
+    }
+
+}
