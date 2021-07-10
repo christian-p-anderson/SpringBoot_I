1fde0e78ff9168e598fe21f90af4fbe03a414750
christian-p-anderson <christian.p.anderson1@gmail.com>
2021-07-10 15:42:02 -0400

    added PUT functionality for name and email

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -4,6 +4,7 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -38,4 +39,12 @@ public class StudentController {
             @PathVariable("studentId") Long studentId) {
         studentService.deleteStudent(studentId);
     }
+
+    @PutMapping(path = "{studentId")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
+    };
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,9 +3,11 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service //we want to instantiate this in the StudentController class, so we identify it as a spring bean
@@ -42,5 +44,35 @@ public class StudentService {
         }
         studentRepository.deleteById(studentId);
 
+    }
+
+    @Transactional //we don't use any queries below because of @Transactional | using @Transactional puts us
+    //in a managed state
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email) {
+        StudentModel student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id " + studentId + " does not exist"
+        ));
+
+        if (name != null &&
+            name.length() > 0 &&
+            !Objects.equals(student.getName(), name)) {
+                student.setName(name);
+        }
+
+        if (email != null &&
+            email.length() > 0 &&
+            !Objects.equals(student.getEmail(), email)) {
+
+            Optional<StudentModel> studentOptional = studentRepository
+                    .findStudentModelByEmail(email);
+
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
