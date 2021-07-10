27c5c24668e15c0c23e6f65300749b0e292fc560
Christian Anderson <ca96187@gmail.com>
2021-07-10 13:53:56 -0400

    created StudentRepository interface to extend JpaRepository

---
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.student;
+
+//this interface is responsible for data access | it's the data access layer
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<StudentModel, Long> {
+}
