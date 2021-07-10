30899538ded23997454cb72f5a5cf5c517c9eea9
Christian Anderson <ca96187@gmail.com>
2021-07-10 13:50:57 -0400

    defined what getStudents is allowed to do

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,32 +1,23 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-@Component //we want to instantiate this in the StudentController class, so we identify it as a spring bean
+@Service //we want to instantiate this in the StudentController class, so we identify it as a spring bean
 public class StudentService {
 
-    public List<StudentModel> getStudents() {
-        return List.of(
+    private final StudentRepository studentRepository;
 
-                new StudentModel(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                ),
-                new StudentModel(
-                        2L,
-                        "Christian",
-                        "ca@gmail.com",
-                        LocalDate.of(2000, Month.JUNE, 9),
-                        21
-                )
-        );
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
     }
 
+    public List<StudentModel> getStudents() {
+        return studentRepository.findAll();
+    }
 }
